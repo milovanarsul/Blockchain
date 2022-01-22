c78c421f1d87c1eb7ada7e42e1dd1b8b206e89ac
Marian Arsul <marian.arsul00@e-uvt.ro>
2022-01-22 17:29:12 +0200

    Run 2

--- main.js
+++ main.js
@@ -35,10 +35,40 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash(); //calculate the hash of the new block
         this.chain.push(newBlock); //add the new block to the chain
     }
+
+    //verifiy the integity of the blockchain
+    //return true if the blockchain is valid, otherwise false
+    isChainValid(){
+        for(let i = 1; i < this.chain.length-1; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            //check if the hash of the current block is still valid
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            //check if our block points to the correct previous block
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
+/*
 let swiftCoin = new Blockchain();
 swiftCoin.addBlock(new Block(1, "22/01/2022", {amount: 4}));
 swiftCoin.addBlock(new Block(2, "22/01/2022", {amount: 10}));
 
 console.log(JSON.stringify(swiftCoin, null, 4));
+*/
+
+let swiftCoin = new Blockchain();
+swiftCoin.addBlock(new Block(1, "22/01/2022", {amount: 4}));
+swiftCoin.addBlock(new Block(2, "22/01/2022", {amount: 10}));
+
+console.log('Is blockchain avalid? ' + swiftCoin.isChainValid());
+
